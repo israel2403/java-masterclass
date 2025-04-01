7054f44056d019c3581e9f0b9be86c5158dc8110
Israel Huerta <israelhf24@gmail.com>
2025-04-01 11:43:28 -0600

    Enhance Main class with age calculations and formatted output

--- src/main/java/com/huerta/section_7/_02_text_blocks/Main.java
+++ src/main/java/com/huerta/section_7/_02_text_blocks/Main.java
@@ -12,5 +12,23 @@ public class Main {
           \u2022 First Point
             \u2022 Sub Point""";
     System.out.println(textBlock);
+
+    final int age = 30;
+    System.out.printf("Your age is %d%n", age);
+
+    final int yearOfBirth = 2025 - age;
+    System.out.printf("Age = %d, Your year of birth is %d%n", age, yearOfBirth);
+
+    System.out.printf("Your age is %.2f%n", (float) age);
+
+    for (int i = 1; i <= 100000; i *= 10) {
+      System.out.printf("Printing %6d%n", i);
+    }
+
+    String formattedSTring = String.format("Your age is %d", age);
+    System.out.println(formattedSTring);
+
+    formattedSTring = "Your age is %d".formatted(age);
+    System.out.println(formattedSTring);
   }
 }
