4dd2988bb77f2d23f08f0c4fb7e88ba91911a33c
Israel Huerta <israelhf24@gmail.com>
2025-04-02 11:41:51 -0600

    Implement Main class with string manipulation methods and formatted output

---
+++ src/main/java/com/huerta/section_7/_03_all_about_strings/Main.java
@@ -0,0 +1,34 @@
+package com.huerta.section_7._03_all_about_strings;
+
+public class Main {
+  public static void main(String[] args) {
+    printInformation("Hello World");
+    printInformation("");
+    printInformation("\t   \n");
+
+    String helloWorld = "Hello World";
+    System.out.printf("index of 'r' in %s is %d%n", helloWorld, helloWorld.indexOf('r'));
+
+    System.out.printf("index of 'World' in %s is %d%n", helloWorld, helloWorld.indexOf("World"));
+    System.out.printf("index of 'l' in %s is %d%n", helloWorld, helloWorld.lastIndexOf("l"));
+
+    System.out.printf("index of 'World' in %s is %d%n", helloWorld, helloWorld.indexOf("l", 3));
+    System.out.printf("index of 'l' in %s is %d%n", helloWorld, helloWorld.lastIndexOf("l", 8));
+  }
+
+  public static void printInformation(final String string) {
+    final int length = string.length();
+    System.out.printf("Length of %s is %d%n", string, length);
+    if (string.isEmpty()) {
+      System.out.println("String is empty");
+      return;
+    }
+    if (string.isBlank()) {
+      System.out.println("String is blank");
+
+    }
+    System.out.printf("First character of %s is %c%n", string, string.charAt(0));
+
+    System.out.printf("Last character of %s is %c%n", string, string.charAt(length - 1));
+  }
+}
