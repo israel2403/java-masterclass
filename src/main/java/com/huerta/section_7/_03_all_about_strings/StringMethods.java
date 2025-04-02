eedfd84eb585dcf9a249c96efe7b25eebda39928
Israel Huerta <israelhf24@gmail.com>
2025-04-02 12:01:36 -0600

    Add StringMethods class for date manipulation and formatted output

---
+++ src/main/java/com/huerta/section_7/_03_all_about_strings/StringMethods.java
@@ -0,0 +1,16 @@
+package com.huerta.section_7._03_all_about_strings;
+
+public class StringMethods {
+  public static void main(String[] args) {
+
+    final String birthday = "25/11/1982";
+    final int startingIndex = birthday.indexOf("1982");
+    System.out.printf("Starting index of 1982 in %s is %d%n", birthday, startingIndex);
+    System.out.printf("Birth year in %s is %s%n", birthday, birthday.substring(startingIndex));
+
+    System.out.printf("Month in %s is %s%n", birthday, birthday.substring(3, 5));
+
+    final String newDate = String.join("/", "25", "11", "1982");
+    System.out.printf("New date is %s%n", newDate);
+  }
+}
