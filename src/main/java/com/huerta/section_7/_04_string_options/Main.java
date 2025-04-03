2460b76b7d6833b67a2c7c5a2de43961cd72292b
Israel Huerta <israelhf24@gmail.com>
2025-04-02 19:19:35 -0600

    Add string and StringBuilder examples with print information method

--- src/main/java/com/huerta/section_7/_04_string_options/Main.java
+++ src/main/java/com/huerta/section_7/_04_string_options/Main.java
@@ -2,7 +2,20 @@ package com.huerta.section_7._04_string_options;
 
 public class Main {
   public static void main(String[] args) {
+    final String helloWorld = "Hello" + " World";
+    helloWorld.concat(" and Goodbye");
 
+    final StringBuilder helloWorldStringBuilder = new StringBuilder("Hello" + " World");
+    helloWorldStringBuilder.append(" and Goodbye");
+
+    printInformation(helloWorld);
+    printInformation(helloWorldStringBuilder);
+
+    final StringBuilder emptyStart = new StringBuilder();
+    final StringBuilder emptyStart32 = new StringBuilder(32);
+
+    printInformation(emptyStart);
+    printInformation(emptyStart32);
   }
 
   public static void printInformation(final String string) {
@@ -13,5 +26,6 @@ public class Main {
   public static void printInformation(final StringBuilder stringBuilder) {
     System.out.println("StringBuilder = " + stringBuilder);
     System.out.println("Length = " + stringBuilder.length());
+    System.out.println("Capacity = " + stringBuilder.capacity());
   }
 }
