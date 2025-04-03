2243e2cf276f2372b229fbd4e7fc5f5c1a271809
Israel Huerta <israelhf24@gmail.com>
2025-04-02 19:05:19 -0600

    Enhance StringMethods class with additional string manipulation methods and add .gitignore for class files

--- src/main/java/com/huerta/section_7/_03_all_about_strings/StringMethods.java
+++ src/main/java/com/huerta/section_7/_03_all_about_strings/StringMethods.java
@@ -28,5 +28,22 @@ public class StringMethods {
 
     // replace caratacter with replace method
     System.out.printf("New date is %s%n", newDate.replace("/", "-"));
+
+    // replace frst character with replaceFirst method
+    System.out.printf("New date is %s%n", newDate.replaceFirst("/", "-"));
+
+    // replace all characters with replaceAll method
+    System.out.printf("New date is %s%n", newDate.replaceAll("/", "---"));
+
+    // use repeat method
+    System.out.println("ABC\n".repeat(3));
+    System.out.println("-".repeat(20));
+
+    // use indent method
+    System.out.println("ABC\n".repeat(3).indent(8));
+    System.out.println("-".repeat(20));
+
+    System.out.println("    ABC\n".repeat(3).indent(-2));
+    System.out.println("-".repeat(20));
   }
 }
