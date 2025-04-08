a7c862b45b56a6e95cb48a282ff3e1ca3bc3e875
Israel Huerta <israelhf24@gmail.com>
2025-04-08 10:47:48 -0600

    Remove player demonstration code from Main class

--- src/main/java/com/secion_8/_04_encapsulation/Main.java
+++ src/main/java/com/secion_8/_04_encapsulation/Main.java
@@ -2,16 +2,6 @@ package com.secion_8._04_encapsulation;
 
 public class Main {
   public static void main(String[] args) {
-    final Player player = new Player();
-    player.name = "Tim";
-    player.health = 20;
-    player.weapon = "Sword";
 
-    final int damage = 10;
-    player.loseHealth(damage);
-    System.out.println("Player health remaining: " + player.healthRemaining());
-
-    player.loseHealth(11);
-    System.out.println("Player health remaining: " + player.healthRemaining());
   }
 }
