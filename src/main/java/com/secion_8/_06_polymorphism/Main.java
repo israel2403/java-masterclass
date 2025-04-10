f080a6bf2a866bc5543055d91f351a15cd60ac0f
Israel Huerta <israelhf24@gmail.com>
2025-04-09 18:42:50 -0600

    Refactor Main class to implement user input for dynamic movie selection

--- src/main/java/com/secion_8/_06_polymorphism/Main.java
+++ src/main/java/com/secion_8/_06_polymorphism/Main.java
@@ -1,8 +1,21 @@
 package com.secion_8._06_polymorphism;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    final Movie movie = Movie.getMovie("Adventure", "The Hobbit");
-    movie.watchMovie();
+    final Scanner s = new Scanner(System.in);
+    while (true) {
+      System.out.println("Enter Type (A for Adventure, C for Comedy, S for Science Fiction) or Q to quit:");
+      String type = s.nextLine();
+      if ("Qq".contains(type)) {
+        System.out.println("Goodbye!");
+        break;
+      }
+      System.out.println("Enter Movie Title:");
+      final String title = s.nextLine();
+      final Movie movie = Movie.getMovie(type, title);
+      movie.watchMovie();
+    }
   }
 }
