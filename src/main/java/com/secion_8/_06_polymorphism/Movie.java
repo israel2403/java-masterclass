df89761a416ab3f76d95c5383375cec4ec4d0704
Israel Huerta <israelhf24@gmail.com>
2025-04-09 18:12:10 -0600

    Add ComedyMovie and ScienceFiction classes with enhanced watchMovie methods for improved storytelling

--- src/main/java/com/secion_8/_06_polymorphism/Movie.java
+++ src/main/java/com/secion_8/_06_polymorphism/Movie.java
@@ -32,6 +32,18 @@ class ComedyMovie extends Movie {
   @Override
   public void watchMovie() {
     super.watchMovie();
-    System.out.printf(".. %s%n".repeat(3), "Some Funny Happens", "Happy Ending");
+    System.out.printf(".. %s%n".repeat(3), "Some Funny Happens", "Happy Ending", "Happy Ending");
+  }
+}
+
+class ScienceFiction extends Movie {
+  public ScienceFiction(String title) {
+    super(title);
+  }
+
+  @Override
+  public void watchMovie() {
+    super.watchMovie();
+    System.out.printf(".. %s%n".repeat(3), "Bad Aliens do Bad Stuff", "Space Guys Chase Aliens", "Planet Blows Up");
   }
 }
