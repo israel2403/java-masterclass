4dc76fef3ac9eba5e9c54b06028c73905068a49d
Israel Huerta <israelhf24@gmail.com>
2025-04-14 11:33:32 -0600

    Enhance ElectricCar and HybridCar classes with engine start and run methods for improved functionality

--- src/main/java/com/secion_8/_07_polymorphism_challenge/Car.java
+++ src/main/java/com/secion_8/_07_polymorphism_challenge/Car.java
@@ -62,20 +62,41 @@ class ElectricCar extends Car {
     this.avgKmPerCharge = avgKmPerCharge;
     this.batterySize = batterySize;
   }
+
+  @Override
+  public void startEngine() {
+    System.out.printf("BEV -> switch  %d kwh battery on, Ready!%n", batterySize);
+  }
+
+  @Override
+  protected void runEngine() {
+    System.out.printf("BEV -> usage under the average of %.2f km/litre%n", avgKmPerCharge);
+  }
 }
 
 @Getter
 class HybridCar extends Car {
 
-  private double avgKmPerLitre;
+  private double avgKmPerLiter;
   private int batterySize;
   private int cylinders;
 
   public HybridCar(final String description, final double avgKmPerLitre, final int cylinders,
       final int batterySize) {
     super(description);
-    this.avgKmPerLitre = avgKmPerLitre;
+    this.avgKmPerLiter = avgKmPerLitre;
     this.cylinders = cylinders;
     this.batterySize = batterySize;
+  }
+
+  @Override
+  public void startEngine() {
+    System.out.printf("Hybrid -> All %d cylinders are fired up, Ready!%n", cylinders);
+    System.out.printf("Hybrid -> switch  %d kwh battery on, Ready!%n", batterySize);
+  }
+
+  @Override
+  protected void runEngine() {
+    System.out.printf("Hybrid -> usage below average of %.2f km/litre%n", avgKmPerLiter);
   }
 }
